69571998402c64c7f9efa091e4ada10bf11467b8
feitianhang123 <feitianhang@126.com>
2026-04-02 15:57:27 +0800

    修复JSON文件路径：使用绝对路径file:///android_asset/www/

--- android-project/app/src/main/java/com/lvshentong/app/MainActivity.java
+++ android-project/app/src/main/java/com/lvshentong/app/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends Activity {
         });
         
         // Load local HTML file
-        webView.loadUrl("file:///android_asset/www/fetch_test.html");
+        webView.loadUrl("file:///android_asset/www/index_fixed.html");
     }
     
     @Override
