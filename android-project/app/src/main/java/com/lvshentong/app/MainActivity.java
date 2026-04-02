734d6586085bf14da3491dfb692ab0f23b18c1c7
feitianhang123 <feitianhang@126.com>
2026-04-02 14:29:31 +0800

    添加JSON文件加载调试页面

--- android-project/app/src/main/java/com/lvshentong/app/MainActivity.java
+++ android-project/app/src/main/java/com/lvshentong/app/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends Activity {
         });
         
         // Load local HTML file
-        webView.loadUrl("file:///android_asset/www/index_fixed.html");
+        webView.loadUrl("file:///android_asset/www/debug_test.html");
     }
     
     @Override
