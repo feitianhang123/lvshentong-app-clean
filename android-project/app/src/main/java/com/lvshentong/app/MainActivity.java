fc343208b21138f7da96c51942947a7025417c86
feitianhang123 <feitianhang@126.com>
2026-04-02 21:10:00 +0800

    修改WebView加载页面：从module1_debug.html改为module1_fixed_final_v4.html

--- android-project/app/src/main/java/com/lvshentong/app/MainActivity.java
+++ android-project/app/src/main/java/com/lvshentong/app/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends Activity {
         });
         
         // Load local HTML file
-        webView.loadUrl("file:///android_asset/www/module1_debug.html");
+        webView.loadUrl("file:///android_asset/www/module1_fixed_final_v4.html");
     }
     
     @Override
