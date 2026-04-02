3db1094d5135648c975846c9c07819981440e14b
feitianhang123 <feitianhang@126.com>
2026-04-02 15:25:51 +0800

    修复字符串引号缺失问题

--- android-project/app/src/main/java/com/lvshentong/app/MainActivity.java
+++ android-project/app/src/main/java/com/lvshentong/app/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends Activity {
         });
         
         // Load local HTML file
-        webView.loadUrl(file:///android_asset/www/module1_debug.html");
+        webView.loadUrl("file:///android_asset/www/module1_debug.html");
     }
     
     @Override
