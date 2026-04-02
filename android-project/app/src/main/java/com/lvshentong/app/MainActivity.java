fde5875e3c4d7ee43bf6d36b873c672f975f3fee
feitianhang123 <feitianhang@126.com>
2026-04-02 21:22:40 +0800

    添加简单测试页面用于诊断空白问题

--- android-project/app/src/main/java/com/lvshentong/app/MainActivity.java
+++ android-project/app/src/main/java/com/lvshentong/app/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends Activity {
         });
         
         // Load local HTML file
-        webView.loadUrl("file:///android_asset/www/module1_fixed_final_v4.html");
+        webView.loadUrl("file:///android_asset/www/simple_test.html");
     }
     
     @Override
