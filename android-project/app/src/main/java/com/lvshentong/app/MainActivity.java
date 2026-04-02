a8c8d69cb3c6720a761bab32383525e0e7267d23
feitianhang123 <feitianhang@126.com>
2026-04-02 21:52:43 +0800

    添加简单测试页面用于诊断模块一空白问题

--- android-project/app/src/main/java/com/lvshentong/app/MainActivity.java
+++ android-project/app/src/main/java/com/lvshentong/app/MainActivity.java
@@ -55,7 +55,7 @@ public class MainActivity extends Activity {
         });
         
         // Load local HTML file
-        webView.loadUrl("file:///android_asset/www/module1_clean.html");
+        webView.loadUrl("file:///android_asset/www/module1_simple.html");
     }
     
     @Override
